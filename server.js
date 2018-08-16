533d0e099994bba9dc787ad1f0349224c5545636
User <User>
2018-08-15 18:47:38 -0700

    Restructuring website to utilize handlebars templating

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require("path")
+const handlebars = require('handlebars')
+const fs = require("fs")
+
+var template = fs.readFileSync('./ProductView.html', "utf8");
+var data = JSON.parse(fs.readFileSync("./ProImagingTemplate.json"))
+var compileTemplate = handlebars.compile(template);
+var finalPageHTML = compileTemplate(data);
+
 
 app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.sendFile(path.join( __dirname +'/index.html')));
+//app.get('/', (req, res) => res.sendFile(path.join( __dirname +'/index.html')));
+
+app.get('/', (req, res) => 
+res.send(finalPageHTML));
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
