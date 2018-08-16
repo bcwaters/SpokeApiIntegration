e806511b6b44a1b6761f9d5c2ba91ca2758a9e2f
User <User>
2018-08-15 20:50:40 -0700

    set up server to connect to local postgresql database and output table content

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require("path")
 const handlebars = require('handlebars')
 const fs = require("fs")
+const pg = require("pg")
 
 var template = fs.readFileSync('./ProductView.html', "utf8");
 var data = JSON.parse(fs.readFileSync("./ProImagingTemplate.json"))
@@ -10,8 +12,23 @@ var compileTemplate = handlebars.compile(template);
 var finalPageHTML = compileTemplate(data);
 
 
+var isLocal = process.env.DATABASE_LOCAL == "true"
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: !isLocal,
+});
+
+client.connect();
+client.query('SELECT * FROM product;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+  
+
 app.use(express.static(__dirname + '/public'));
-//app.get('/', (req, res) => res.sendFile(path.join( __dirname +'/index.html')));
 
 app.get('/', (req, res) => 
 res.send(finalPageHTML));
@@ -19,3 +36,4 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+  
