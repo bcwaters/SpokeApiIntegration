d149b8c72aa3803c999e38ba6a68d027d150847b
User <User>
2018-08-16 19:16:30 -0700

    Set up route to verify heroku PGdatabase is correctly.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const pg = require("pg")
 var template = fs.readFileSync('./ProductView.html', "utf8");
 var data = JSON.parse(fs.readFileSync("./ProImagingTemplate.json"))
 var compileTemplate = handlebars.compile(template);
-var finalPageHTML = compileTemplate(data);
+
 
 
 var isLocal = process.env.DATABASE_LOCAL == "true"
@@ -18,21 +18,31 @@ const client = new pg.Client({
   ssl: !isLocal,
 });
 
-if(isLocal){
 client.connect();
-client.query('SELECT * FROM product;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-}
+var testString;
+
+
+
+var finalPageHTML = compileTemplate(data);
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => 
-res.send(finalPageHTML));
+res.send(finalPageHTML))
+
+
+app.get('/test', (req, res, next) => {
+	client.query('SELECT * FROM product;', (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.send(result.rows[0])
+	})
+});
+
+
+
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
